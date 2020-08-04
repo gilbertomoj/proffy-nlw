f866830d62f22959a835dfc50e098c351fdbae07
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-03 23:42:13 -0300

    CSS mobile completo*

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -6,7 +6,7 @@ import studyIcons from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
-
+import './styles.css'
 
 function Landing(){
     return (
@@ -18,18 +18,18 @@ function Landing(){
                 </div>
                 <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <a href="w" className="study">
                         <img src={studyIcons} alt="Estudar"/>
                         Estudar
                     </a>
-                    <a href="" className="study">
+                    <a href="w" className="give-classes">
                         <img src={giveClassesIcon} alt="Estudar"/>
                         Estudar
                     </a>
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt=""/>
                 </span>
             </div>
         </div>
