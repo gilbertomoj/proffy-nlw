ccf1c9e16f5b6c0670c70499380f6b5fad2d7319
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-04 01:32:26 -0300

    Exact inserido

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -8,7 +8,7 @@ import TeacherList from './pages/TeacherList';
 function Routes(){
     return(
         <BrowserRouter>
-            <Route path="/" component={Landing} />
+            <Route path="/" exact component={Landing} />
             <Route path="/study" component={TeacherList} />
             <Route path="/give-classes" component={TeacherForm} />
 
