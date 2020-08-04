0b54d290f0f7ffefe7c2d97d75cc32e3271092b2
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-03 23:09:30 -0300

    Elementos implementados

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+
+import studyIcons from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+
 
 function Landing(){
     return (
@@ -7,7 +14,23 @@ function Landing(){
             <div id="page-landing-content" className="container">
                 <div className="logo-container">
                     <img src={logoImg}  alt="Proffy"/>
+                    <h2>Sua plataforma de estudos online.</h2>
                 </div>
+                <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
+                <div className="buttons-container">
+                    <a href="" className="study">
+                        <img src={studyIcons} alt="Estudar"/>
+                        Estudar
+                    </a>
+                    <a href="" className="study">
+                        <img src={giveClassesIcon} alt="Estudar"/>
+                        Estudar
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                </span>
             </div>
         </div>
     )
