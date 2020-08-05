33a1991ea37507147111ce9e31d5606677faa0ce
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-05 01:58:19 -0300

    Epsodio 1 completo

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,8 @@ import PageHeader from '../../components/Page-Header';
 
 import './styles.css';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 function TeacherList(){
     return(
         <div id="page-teacher-list" className="container">
@@ -22,6 +24,32 @@ function TeacherList(){
                     </div>
                 </form>
             </PageHeader>
+            <main>
+                <article className="teacher-item">
+                    <header>
+                    <img src="https://avatars2.githubusercontent.com/u/38990510?s=400&u=9a67ab85804229db6ec973f2a0c56b698d259eee&v=4" alt="Jeremias"/>
+                    <div>
+                        <strong>Jeremias Oliveira</strong>
+                        <span>Informatica</span>
+                    </div>
+                    </header>
+                    <p>
+                    Acredita piamente que a educação pode mudar vidas e passo a passo.
+                    <br/> <br/>
+                    Leva minha vida nesse trilho para me transformar e inspirar pessoas a alcançar também sua própria mudança.
+                    </p>
+                    <footer>
+                        <p>
+                            Preço por hora
+                            <strong>R$ 15,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="Whatsapp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
        
     )
