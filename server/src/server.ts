237cefde74fa13f8eb201b9243406e95cd1428e3
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-05 18:22:15 -0300

    feature(server) :  Conexão com o banco

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
 import express, { response } from 'express';
 const app = express();
 
+app.use(express.json());
 
 app.get("/users", (request , response)=>{
-    response.json({'Roberto' :"132123123"})
+    response.json({'Roberto' :"13212312s3"})
+})
+
+app.post('/users',(request , response)=>{
+    console.log(request.body);
+    return response.json({'Ola':'Olar'})
 })
 
 app.listen(3333);
