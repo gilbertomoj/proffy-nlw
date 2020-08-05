131649b0ab2a85118efee6d437a5cf900f2ea626
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-05 10:53:11 -0300

    Criação de component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PageHeader from '../../components/Page-Header';
 
+import TeacherItem from '../../components/TeacherItem';
 import './styles.css';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 
 function TeacherList(){
     return(
@@ -25,30 +26,11 @@ function TeacherList(){
                 </form>
             </PageHeader>
             <main>
-                <article className="teacher-item">
-                    <header>
-                    <img src="https://avatars2.githubusercontent.com/u/38990510?s=400&u=9a67ab85804229db6ec973f2a0c56b698d259eee&v=4" alt="Jeremias"/>
-                    <div>
-                        <strong>Jeremias Oliveira</strong>
-                        <span>Informatica</span>
-                    </div>
-                    </header>
-                    <p>
-                    Acredita piamente que a educação pode mudar vidas e passo a passo.
-                    <br/> <br/>
-                    Leva minha vida nesse trilho para me transformar e inspirar pessoas a alcançar também sua própria mudança.
-                    </p>
-                    <footer>
-                        <p>
-                            Preço por hora
-                            <strong>R$ 15,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Whatsapp"/>
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
+                <TeacherItem/>
+                <TeacherItem/>
+                <TeacherItem/>
+                <TeacherItem/>
+
             </main>
         </div>
        
