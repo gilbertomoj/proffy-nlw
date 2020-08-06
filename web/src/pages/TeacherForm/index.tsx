c846d5ea1c33e088123df5e4d63b084bf6a60878
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-06 10:44:22 -0300

    feature(web): estilização do teacher form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageHeader from '../../components/Page-Header';
+import './styles.css';
 
 function TeacherForm(){
     return(
@@ -7,7 +8,28 @@ function TeacherForm(){
             <PageHeader title="Que incrivel que você quer dar aulas"
             description="O primeiro passe é preencher esse formulário de inscrição"
             />
-            
+        
+        <main>
+            <fieldset>
+                <legend>
+                    Seus dados
+                </legend>
+                <div className="input-block">
+                    <label htmlFor="name">Nome completo</label>
+                    <input type="text" id="name"/>
+                </div>
+                <div className="input-block">
+                    <label htmlFor="avatar">Avatar</label>
+                    <input type="text" id="avatar"/>
+                </div>
+                <div className="input-block">
+                    <label htmlFor="whatsapp">Whatsapp</label>
+                    <input type="text" id="whatsapp"/>
+                </div>
+
+            </fieldset>
+        </main>
+
         </div>
     )
 }
