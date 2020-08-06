2c1c0d7a7a9d86ad75e60a78bdbd23a1f4eccfd5
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-06 00:11:09 -0300

    feature(server): Ajustes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -26,37 +26,43 @@ routes.post('/classes',async (request , response)=>{
    } = request.body;
 
    const trx = await db.transaction();
-
+   try {
+      
    const insertedUsersId = await trx('users').insert({
-       name,
-       avatar,
-       whatsapp,
-       bio,
-   });
+    name,
+    avatar,
+    whatsapp,
+    bio,
+});
 
-   const user_id = insertedUsersId[0];
+const user_id = insertedUsersId[0];
 
-   const insertedClassesIds = await trx('classes').insert({
-       subject,
-       cost,
-       user_id
-   })
+const insertedClassesIds = await trx('classes').insert({
+    subject,
+    cost,
+    user_id
+})
 
-   const class_id = insertedClassesIds[0];
+const class_id = insertedClassesIds[0];
 
-   const classSchedule = schedule.map((scheduleItem: ScheduleItem )=>{
-       return{
-           class_id,
-            week_day: scheduleItem.week_day,
-            from : convertHourToMinutes(scheduleItem.from),
-            to: convertHourToMinutes(scheduleItem.to)
-       };
-   });
+const classSchedule = schedule.map((scheduleItem: ScheduleItem )=>{
+    return{
+        class_id,
+         week_day: scheduleItem.week_day,
+         from : convertHourToMinutes(scheduleItem.from),
+         to: convertHourToMinutes(scheduleItem.to)
+    };
+});
 
-   await trx('class_schedule').insert(classSchedule);
+await trx('class_schedule').insert(classSchedule);
 
-   await trx.commit();
-   return response.send();  
+await trx.commit();
+return response.status(201).send();   
+   } catch (err) {
+       await trx.rollback();
+
+       return response.status(400).json({error : 'Um erro inesperado aconteceu'})
+   }
 
 })
 
