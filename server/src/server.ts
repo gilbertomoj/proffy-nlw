cb78d96c8c6bd66589a2a88ef7c5e4b88cda9fb5
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-05 22:52:24 -0300

    feature(server): Criado o migrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import express, { response } from 'express';
 const app = express();
 
-app.use(express.json());
+app.use(express.json());//
 
 app.get("/users", (request , response)=>{
     response.json({'Roberto' :"13212312s3"})
