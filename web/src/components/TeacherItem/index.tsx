ba0c78b2c2051f770b2722a0c99f0473492a5c66
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-08 17:32:58 -0300

    update

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
 export interface Teacher{
     
@@ -18,7 +19,14 @@ export interface Teacher{
     teacher: Teacher;
 }
 
+
 const TeacherItem:React.FC<TeacherItemProps> = ({teacher})=> {
+    
+function createNewConnection(){
+    api.post('connections',{
+        user_id: teacher.id,
+    })
+}
     return(
 <article className="teacher-item">
 <header>
@@ -36,7 +44,7 @@ const TeacherItem:React.FC<TeacherItemProps> = ({teacher})=> {
         Preço por hora
         <strong>R$ {teacher.cost}</strong>
     </p>
-    <a href={`https://wa.me/${teacher.whatsapp}`}>
+    <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
         <img src={whatsappIcon} alt="Whatsapp"/>
         Entrar em contato
     </a>   
