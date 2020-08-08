d33b41cfbece18b23254676bc7719c7806b42895
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-08 10:01:38 -0300

    feature(web) : Conexão do connections com a landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState , useEffect} from 'react';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 
@@ -9,8 +9,20 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
 function Landing(){
+    const [totalConnections , setTotalConnections] = useState(0);
+
+    useEffect(()=>{
+        api.get('connections').then(response =>{
+            const {total} =     response.data;
+
+            setTotalConnections(total);
+             
+        })
+    }, []) 
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -31,7 +43,7 @@ function Landing(){
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt=""/>
+                    Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt=""/>
                 </span>
             </div>
         </div>
