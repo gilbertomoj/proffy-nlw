56526f32f090f5b3c406e533c014130815c54988
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-08 18:27:48 -0300

    update

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -44,7 +44,7 @@ function createNewConnection(){
         Preço por hora
         <strong>R$ {teacher.cost}</strong>
     </p>
-    <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
+    <a target="_blank" rel="noopener noreferrer" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
         <img src={whatsappIcon} alt="Whatsapp"/>
         Entrar em contato
     </a>   
