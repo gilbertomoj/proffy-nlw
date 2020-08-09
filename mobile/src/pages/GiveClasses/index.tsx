fa024ca5d199475a58dbc9e947c9aa5e4d51cd74
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-09 19:59:23 -0300

    feature(mobile): Base estrutural da pagina GiveClasses

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { View , Text } from 'react-native';
+import { View , Text , ImageBackground} from 'react-native';
+import styles from './styles'
+import giveClassesBGI from '../../assets/images/give-classes-background.png';
+
 
 function GiveClasses(){
     return(
-        <View>
-            <Text>bla </Text>
+        <View style={styles.container}>
+            <ImageBackground resizeMode="contain" source={giveClassesBGI} style={styles.content}>
+                <Text style={styles.title}>Quer ser um Proffy ? </Text>
+                <Text style={styles.description}>Para começar, você precisa se cadastrar como professor na nossa plataforma web</Text>
+                
+
+            </ImageBackground>
         </View>
     )
 
 }
+
+//OBS: Bottão com link para o site
 
 export default GiveClasses;
---
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#8257e5',
+        justifyContent: 'center',
+        padding: 20,
+
+    },
+    content:{
+        flex:1,
+        justifyContent:'center',
+    }
+});
+
+export default styles
