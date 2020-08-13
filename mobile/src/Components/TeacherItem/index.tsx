db8c488e0f81b88c1563379371f112386edad251
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-08-13 01:14:07 -0300

    feature(mobile): Rota para connections feita

--- mobile/src/Components/TeacherItem/index.tsx
+++ mobile/src/Components/TeacherItem/index.tsx
@@ -6,6 +6,7 @@ import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 import AsyncStorage from '@react-native-community/async-storage';
+import api from '../../services/api';
 
 
 export interface Teacher{
@@ -28,6 +29,11 @@ const TeacherItem:React.FC<TeacherItemProps>=({ teacher, favorited })=>{
     const [isFavorited, setIsFavorited] = useState(favorited);
 
     function handleLinkToWhatsapp(){
+
+        api.post('connections',{
+            user_id: teacher.id,
+        })
+
         Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
     }
     async function handleToggleFavorite(){
